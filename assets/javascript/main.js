f31be92c222f025f017558c83d4400d7b32a9770
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-07-25 08:23:51 -0700

    Scrolling

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -31,5 +31,11 @@ $(document).ready(function(){
           }
         });
       });
+      $(".scroll").click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+          scrollTop: $(this.hash).offset().top
+        }, 1000 );
+      });
    
 });
